906f37012b887b28eedc9aeb1d160c983040bad8
kwonsh001 <kwonsh001@bluedigm.com>
2024-10-13 17:27:46 +0900

    fix: 포트변경

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def login():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # 테이블 생성
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
